8a7485c6b61624ad497e33cf3b7bc097db19814c
SubN3o <sub.n3o@gmail.com>
2019-01-03 23:16:53 +0100

    My Hello World

---
+++ 3LDeco/src/TroisLDecoBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TroisLDecoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('@TroisLDeco/index.html.twig');
+    }
+}
---
+++ 3LDeco/src/TroisLDecoBundle/TroisLDecoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TroisLDecoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TroisLDecoBundle extends Bundle
+{
+}
