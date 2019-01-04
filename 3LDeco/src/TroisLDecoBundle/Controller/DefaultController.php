bef289a9f5d887fbd05944af8b4bedc774f363f3
SubN3o <sub.n3o@gmail.com>
2019-01-05 00:52:57 +0100

    Html OK

--- 3LDeco/src/TroisLDecoBundle/Controller/DefaultController.php
+++ 3LDeco/src/TroisLDecoBundle/Controller/DefaultController.php
@@ -8,11 +8,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
-    	$test = "hello";
+    	$test = "Accueil";
         return $this->render('@TroisLDeco/index.html.twig', ['test'=>$test]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction()
+    {
+    	return $this->render('@TroisLDeco/admin/admin.html.twig');
     }
 }
